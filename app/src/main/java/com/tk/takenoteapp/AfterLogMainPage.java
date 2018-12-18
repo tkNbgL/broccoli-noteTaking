8a2496de168e0b71e1f1e248f348a89c5177e5c2
Utku Nebioğlu <utku.nebioglu@gmail.com>
2018-12-19 00:22:50 +0300

    creating of activities

---
+++ app/src/main/java/com/tk/takenoteapp/AfterLogMainPage.java
@@ -0,0 +1,13 @@
+package com.tk.takenoteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AfterLogMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_log_main_page);
+    }
+}
