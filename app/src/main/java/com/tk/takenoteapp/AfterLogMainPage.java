d96d2550db8c5732cb17bc9fef2464c6eb725f81
Utku Nebioğlu <utku.nebioglu@gmail.com>
2018-12-20 18:43:04 +0300

    bottom and top app bars were added

--- app/src/main/java/com/tk/takenoteapp/AfterLogMainPage.java
+++ app/src/main/java/com/tk/takenoteapp/AfterLogMainPage.java
@@ -1,7 +1,11 @@
 package com.tk.takenoteapp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AfterLogMainPage extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class AfterLogMainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_log_main_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.searc_app_bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_search){
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
