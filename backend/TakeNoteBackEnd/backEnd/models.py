243b9ad1ea8f090a68a7cec185f901dd0422136a
Utku Nebioğlu <utku.nebioglu@gmail.com>
2019-01-14 20:38:19 +0300

    backEnd was erased, manifest file was edited

--- backend/TakeNoteBackEnd/backEnd/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class TextNote(models.Model):
-	subject = models.CharField(max_length=20)
-	note = models.CharField(max_length=200)
--- backend/TakeNoteBackEnd/backEnd/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from backEnd.models import TextNote
-
-class TextNoteSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = TextNote
-		fields = ('id', 'subject', 'note')
